a043c33f4d58be2627d7e4acf21eb91b2c1c4e2c
dima <andrushkodima@gmail.com>
2023-10-24 16:54:54 +0300

    Add

--- app/add-post/page.tsx
+++ app/add-post/page.tsx
@@ -8,25 +8,24 @@ export const metadata: Metadata = {
   description: 'Meta blog | Add new post',
 };
 
-// const getCategories = async (): Promise<{ categories: ICategory[] }> => {
-//   const res = await fetch(`${process.env.APP_URL}/api/categories`, {
-//     cache: 'no-store',
-//   });
+const getCategories = async (): Promise<{ categories: ICategory[] }> => {
+  const res = await fetch(`${process.env.APP_URL}/api/categories`, {
+    cache: 'no-store',
+  });
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
-//   return res.json();
-// };
+  return res.json();
+};
 
 const AddPost = async () => {
-  // const { categories } = await getCategories();
+  const { categories } = await getCategories();
 
   return (
     <div className={styles.addPost}>
-      {/* <Editor  categories={categories} /> */}
-      <Editor  categories={[]} />
+      <Editor categories={categories} />
     </div>
   );
 };
